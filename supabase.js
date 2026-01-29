15722cda8f507703c73c9577153caa05cad4eacc
alexela11 <alexis.r.cueto@gmail.com>
2026-01-29 11:52:27 +0800

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+const SUPABASE_URL = "YOUR_NEW_PROJECT_URL";
+const SUPABASE_KEY = "YOUR_ANON_KEY";
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+</script>
